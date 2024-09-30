71d4227d6af243676637261c4192981c7970debd
moganesan <developer.moganesan@gmail.com>
2024-09-30 15:01:44 +0530

    fullMath library updated

--- contracts/SwapBot.sol
+++ contracts/SwapBot.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IMulticall.sol";
-import "@uniswap/v3-core/contracts/libraries/FullMath.sol";
+import "./libraries/FullMath.sol";
 import "./Interfaces/IQuoter.sol";
 import "./Interfaces/ISwapRouterV2.sol";
 import "./Interfaces/ISwapRouterV3.sol";
